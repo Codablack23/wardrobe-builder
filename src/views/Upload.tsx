9ced4021760d5b25b405ea6e9287241405d0d65e
Edih Goodluck <77158925+Codablack23@users.noreply.github.com>
2023-09-22 11:28:57 +0100

    Update Upload.tsx

--- src/views/Upload.tsx
+++ src/views/Upload.tsx
@@ -1,11 +1,9 @@
-import UploadDropZone from "../components/UploadDropZone"
 import "../styles/upload.scss"
 
 export default function Upload(){
     return(
         <div className="upload-container">
           <h3 className="title">Upload</h3>
-          <UploadDropZone/>
         </div>
     )
 }
